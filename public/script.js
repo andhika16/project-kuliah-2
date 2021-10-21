5b57367175e0fc3b984faeb562f6e750a8b6b0d1
andhika16 <moh.andhika16@gmail.com>
2021-10-21 17:12:37 +0700

    mengubah warna

--- public/script.js
+++ public/script.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 0) {
-            $("nav").addClass("md:bg-gradient-to-r from-green-500 to-green-700");  	
+            $("nav").addClass("md:bg-gradient-to-r from-indigo-800 to-indigo-500");  	
             $("nav").removeClass("md:opacity-100");  	
         }
         else {
-            $("nav").removeClass("md:bg-gradient-to-r from-green-500 to-green-700");  	
+            $("nav").removeClass("md:bg-gradient-to-r from-indigo-800 to-indigo-500");  	
             $("nav").addClass("md:opacity-100");
         }
     })
